1ea4e546df7f73fe566bf76e66a9eb9c7a0cae23
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2019-01-08 17:48:44 +0100

    cleanup

---
+++ task3/date_augmentor.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class DateAugmentor(object):
+
+    def augment_dates(self, dates: List[str]) -> List[str]:
+        """
+        TODO: multiple modes:
+        - swap order of elements
+        - edge cases of month and year
+        - invalid dates
+        - dates with letters (e.g. Jan, Feb)
+        - other data (e.g. addresses)
+        - number data that is not dates but might look similar (telephone numbers)
+        """
+        # TODO
+        # multiple modes
+        pass
